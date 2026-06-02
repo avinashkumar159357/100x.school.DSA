fb6de5d9ac480aaf6a4d215416e744ef37ab347b
avinashkumar159357 <aviofficial159357@gmail.com>
2026-06-02 10:48:36 +0530

    Am I Prime?

---
+++ 8-Time_&_Space_complexity/E_Am_i_prime.cpp
@@ -0,0 +1,36 @@
+//File name:Am I Prime?
+//Author:Avinash kumar
+//Email Address: aviofficial159357@gmail.com
+//Problem ID:Time and space complexity (E)
+//Description:practicing to write code with constraint on time and space
+//Last Changed:2-06-2026
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+bool ISprime(ll n){
+    if(n<2) return false;
+    if(n==2) return true;
+    if(n%2==0) return false;
+
+    for (ll i = 3; i*i <=n; i+=2){
+        if(n%i==0)return false;
+    }
+    return true;
+}
+void solve(){  //1<=N<=10^12
+    ll n; cin>>n;
+    if(ISprime(n)) cout<<"YES";
+    else cout<<"NO";
+}
+
+int main() {
+    ios :: sync_with_stdio(0);
+    cin.tie(0);
+    ll q=1; // cin>>q;
+    while (q--)
+    {
+        solve();
+    }
+    
+}
