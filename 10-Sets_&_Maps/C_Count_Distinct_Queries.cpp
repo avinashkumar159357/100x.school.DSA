7d22d9a219cb32681f0efe0381e987399c7f75c6
avinashkumar159357 <aviofficial159357@gmail.com>
2026-06-10 12:46:59 +0530

    Count Distinct Queries

---
+++ 10-Sets_&_Maps/C_Count_Distinct_Queries.cpp
@@ -0,0 +1,42 @@
+//File name:Count Distinct Queries
+//Author:Avinash kumar
+//Email Address: aviofficial159357@gmail.com
+//Problem ID:Sets and Maps (C)
+//Description:Hashing
+//Last Changed:10-06-2026
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve(){ 
+    int q,a=0; cin>>q;
+    map<ll,int>mp;
+    ll x=0;
+    while (q--)
+    {
+        cin>>a;
+
+        if(a==1){
+            cin>>x;
+            mp[x]++;
+        }else if(a==2){
+            cin>>x;
+            if(mp.count(x))mp.erase(x);
+        }else if(a==3){
+            cout<<mp.size()<<"\n";
+        }else {
+            cin>>x;
+            if(mp.find(x)!=mp.end())cout<<"YES\n";
+            else cout<<"NO\n";
+        }
+    }
+    
+}
+
+int main() {
+    ios :: sync_with_stdio(0);
+    cin.tie(0);
+
+    int TC=1; //cin>>TC;
+    while (TC--) {solve();}
+}
